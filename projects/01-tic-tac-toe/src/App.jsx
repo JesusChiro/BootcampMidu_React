d3b68dc02f63fa3f94b89e22cb6ded00c6446e84
JesusChiro <jesus_chiroque@hotmail.com>
2024-01-15 21:50:57 -0500

    agregando el guardar partida en el local storage

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import confetti from "canvas-confetti"
 import { WinnerModal } from "./components/WinnerModal.jsx"
 import { Square } from "./components/Square.jsx"
-import { checkWinnerFrom,checkEndGame  } from "./logic/board.js"
-import { TURNS} from "./constants.js"
+import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
+import { saveGameToStorage, resetGameStorage } from "./logic/storage/index.js"
+import { TURNS } from "./constants.js"
 
 
 function App() {
-
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
 
   //null es que no hay gandador, false es que hay un empate
   const [winner, setWinner] = useState(null)
@@ -18,6 +25,7 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+    resetGameStorage()
   }
 
   const updateBoard = (index) => {
@@ -31,6 +39,11 @@ function App() {
     //cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    //guardar aqui partida
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
     //revisar si hay ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
@@ -40,22 +53,18 @@ function App() {
       setWinner(false)//empate
     }
   }
+
   return (<main className="board">
     <h1>Tic tac toe</h1>
     <button onClick={resetGame}>Reset del juego</button>
     <section className="game">
       {board.map((square, index) => {
         return (
-          <Square
-            key={index}
-            index={index}
-            updateBoard={updateBoard}
-          >
+          <Square key={index} index={index} updateBoard={updateBoard}>
             {square}
           </Square>
         )
-      })
-      }
+      })}
     </section>
 
     <section className="turn">
